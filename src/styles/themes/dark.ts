afe42457301aac5b30263161b915d94d0ea83092
jandsonn <jandsondosanjos@hotmail.com>
2022-02-06 11:06:37 -0300

    Adicionando modelo do dashboard

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#3155B5',
+        secondary: '#5285A3',
+        tertiary: '#7EB4C4',
+
+        white: '#FFFFFF',
+        black: '#202125',
+        gray: '#ADAFAE',
+
+        sucess: '#AD68AA',
+        info: '#F28A20',
+        warning: '#D71C18',
+
+    },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '##DCDCDC',
+        secondary: '#FFFFFF',
+        tertiary: '#F5F5F5',
+
+        white: '#000',
+        black: '#FFF',
+        gray: '#ADAFAE',
+
+        sucess: '##46BC7F',
+        info: '##3577BE',
+        warning: '##F46F20',
+
+    },
+};
